980726c171af66b8e1bfbcdf7d0c3c3128a75046
Mattia Malonni <mattia.malonni@spindox.it>
2025-10-31 17:44:11 +0100

    Add Prisma configuration and dependencies; create schema and client files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body className={`antialiased dark`}>{children}</body>
+      <body className="antialiased dark">{children}</body>
     </html>
   );
 }
